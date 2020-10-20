a17e51f62d0af18a5a2aa895c7bd9904d7cea664
Manuel Sosa <manuel_sosa_martinez@hotmail.com>
2020-10-20 19:50:57 -0300

    initial navigation for our app

--- react_sv/src/components/Match.js
+++ react_sv/src/components/Match.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Match() {
+function Match( {match} ) {
+
+  const game = {name : "Tom riddle's game",
+                id : match.params.id,
+                playerCount : 6};
+
   return (
     <div >
-      <h1> Hola </h1>
-      <Login />
+      <h1> {game.name} </h1>
+      <h2> There are {game.playerCount} players connected</h2>
+      <h4> Game id : {game.id} </h4>
     </div>
   );
 }
--- react_sv/src/components/login.js
+++ react_sv/src/components/login.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch} from "react-redux";
+import {useHistory} from "react-router-dom";
 import { login } from "./../actions/login";
+
 
 function Login() {
   const logged_in = useSelector((state) => state.logged_in);
   const dispatch = useDispatch();
+ 
+  const history = useHistory();
+
+  function handleClick() {
+    dispatch(login());
+    if(!logged_in) history.push("/match");
+    else history.push("/");
+  }
 
   return (
     <div>
-      <h1> {logged_in ? "Logged in!" : "Not logged in"} </h1>
-      <button onClick={() => dispatch(login())}>
+      <h3> {logged_in ? "Logged in!" : "Not logged in"} </h3>
+      <button onClick={ () => {handleClick()}}>
         {logged_in ? "Logout" : "Login"}
       </button>
     </div>
