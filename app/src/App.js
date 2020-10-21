d35970925ff3ac26c90cb31cca71481b517a7914
guidoivetta <guidoivetta@gmail.com>
2020-10-21 16:21:08 -0300

    SV-15 #comment Added CreateMatch route

--- app/src/App.js
+++ app/src/App.js
@@ -5,17 +5,19 @@ import Lobby from "./components/Lobby";
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
 import Match from "./components/Match";
+import CreateMatch from "./components/CreateMatch";
+
 
 function App() {
   return (
     <BrowserRouter>
-    
       <div className="App">
         <Nav />
         <Switch>
           <Route exact path="/" component={Lobby} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/match/create" component={CreateMatch} />
           <Route exact path="/match/:id" component={Match} />
         </Switch>
       </div>
