6984fe0f3f6c38fc7fe7ad3e1423010330c683fa
Matidp <matiasdepascuale@gmail.com>
2020-10-20 21:53:23 -0300

    SV-20 #time 1h #comment Creando Clase User #done
    SV-20 #time 2m #comment added Matches attribute for User class

--- backend/database.py
+++ backend/database.py
@@ -31,5 +31,15 @@ class Board(db.Entity):
     FailedElectionsCount = Optional(int)
     Match = Required(Match)
 
-db.generate_mapping(create_tables=True)  
+
+class User(db.Entity):
+    Id = PrimaryKey(int, auto=True)
+    Email = Required(str, unique=True)
+    Username = Required(str, max_len=30, unique=True)
+    Password = Required(str, hidden=True, max_len=30)
+    Players = Set('Player')
+    Matches = Set('Match')
+
+db.generate_mapping(create_tables=True)
+
 
