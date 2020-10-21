3faac3a030eef10175be71e41013e98ae6f60cf5
Matidp <matiasdepascuale@gmail.com>
2020-10-20 21:53:23 -0300

    SV-20 #time 1h #comment Creando Clase User #done
    SV-20 #time 2m #comment added Matches attribute for User class

--- backend/database.py
+++ backend/database.py
@@ -3,4 +3,14 @@ from pony.orm import *
 db = Database()
 db.bind('sqlite', 'example.sqlite', create_db=True)
 
+class User(db.Entity):
+    Id = PrimaryKey(int, auto=True)
+    Email = Required(str, unique=True)
+    Username = Required(str, max_len=30, unique=True)
+    Password = Required(str, hidden=True, max_len=30)
+    Players = Set('Player')
+    Matches = Set('Match')
+
 db.generate_mapping(create_tables=True)
+
+
