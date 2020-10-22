b57218dedab45b8d39a0861c96c8aef6dc1078f5
Matidp <matiasdepascuale@gmail.com>
2020-10-22 16:05:29 -0300

    SV-34 Metodos agregados

--- backend/crud.py
+++ backend/crud.py
@@ -1,11 +1,34 @@
 from pony.orm import db_session, select, count
+from . import database
 #Falta importar archivos de /backend.
 #Una vez decidido los nombres agregar.
 
 @db_session
-def consulta_user(name, upassword):
+def user_is_registred(name, upassword):
     try:
         u = User.get(Username = name, Password = upassword)
+        if u:
+            return True
+        else: 
+            return False
+    except Exception:
+        return False
+
+@db_session
+def check_username(name):
+    try:
+        u = User.get(Username = name)
+        if u:
+            return True
+        else: 
+            return False
+    except Exception:
+        return False
+
+@db_session
+def check_mail(mail):
+    try:
+        u = User.get(Email = mail)
         if u:
             return True
         else: 
