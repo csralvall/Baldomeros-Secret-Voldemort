bf2018d2065c28b61fb1abc9fceaf1640dd56def
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2020-10-22 20:00:49 -0300

    SV-22 #time 3h #comment register endpoint completed, not tested yet

---
+++ users.py
@@ -0,0 +1,39 @@
+from crud import *
+
+from fastapi import FastAPI, File, Form, UploadFile, HTTPException
+from pydantic import BaseModel, EmailStr, SecretStr
+
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/")
+def db_dump():
+    with db_session:
+        q = select(p for p in db.User)
+        return [p.to_dict() for p in q]
+
+@app.post("/account", tags=["user"])
+async def register_user(
+    email: EmailStr = Form(...),
+    username: str = Form(...),
+    password: SecretStr = Form(...),
+    photo: Optional[UploadFile] = Form(None)):
+
+    user = {"email": email, "username": username}
+
+    if check_email(email):
+        raise HTTPException(status_code=409, detail="Email already registered")
+
+    if check_username(username):
+        raise HTTPException(status_code=409, detail="Username already taken")
+
+    password_ = password.get_secret_value()
+
+    if not create_user(email, username, password_):
+        raise HTTPException(status_code=409, detail="Unable to register user")
+
+    return user
+
+
+
