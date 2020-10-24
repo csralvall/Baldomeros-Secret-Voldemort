06e97915c0949054b87bf1e1f20bf7cbdd4bd643
Feltes <joacofeltes@gmail.com>
2020-10-24 16:43:14 -0300

    SV-21 #time 30m #comment adding tests for the create user function

---
+++ backend/tests/test_SV-21.py
@@ -0,0 +1,31 @@
+import unittest 
+from ..crud import create_user, delete_data
+from ..model import *
+
+class TestCrud(unittest.TestCase):
+
+    def setUp(self):
+        delete_data(User)
+         
+    def test_first(self):
+        self.assertTrue(create_user("joaco@gmail.com","joaco","kajdsflaskfdjsla"))
+
+    def test_second(self):
+        self.assertFalse(create_user("joa@gmail.com","","hola"))
+
+    
+    def test_third(self):
+        self.assertFalse(create_user("","joaaaa","holaasdsad"))
+
+    def test_fourth(self):
+        self.assertFalse(create_user("joafdsa@gmail.com","",""))
+
+    def test_fifth(self):
+        self.assertTrue(create_user("joaco1@gmail.com","joaco1","kajdsflaskfdjsla"))
+
+    def test_drop(self):
+        self.assertTrue(create_user("joaco@gl.com","co","kajdsflaskfdjsla"))
+
+
+if __name__ == '__main__':
+    unittest.main()
