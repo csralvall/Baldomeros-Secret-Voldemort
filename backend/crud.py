1bcb8b670aba44078ce48b5a42123a6571c44e99
joaquinfeltes <51490191+joaquinfeltes@users.noreply.github.com>
2020-10-26 11:47:01 -0300

    Feature/sv 21 registro bd (#4)
    * SV-21 #time 20m #comment added function to add users to the database
    
    * SV-21 #time 5m #comment adding init files for packaging and imports
    
    * SV-21 #time 10m #comment adding delete_data function, to clear the data in a table for tests
    
    * SV-21 #time 30m #comment adding tests for the create user function
    
    * SV-21 #time 5m #comment adding requierements.txt for pony installation, new files in gitignore
    
    * SV-21 #time 2m #comment import file name changed
    
    * SV-21 #time 2m #comment import file name changed in test
    
    * SV-21 #time 2m #comment main.py deleted for merge conflicts
    
    * SV-51 #time 10m #comment setting database, nothing to test. #done
    
    * SV-20 #time 1h #comment Creando Clase User #done
    
    SV-20 #time 2m #comment added Matches attribute for User class
    
    * SV-22 #time 30m #comment Register endpoint tested and reviewed. #done
    
    SV-22 #time 3h #comment register endpoint completed, not tested yet
    
    SV-22 #time 1m #comment Format changes
    
    * SV-23 #time 2h #comment Login endpoint tested and reviewed. #done" (#2)
    
    * Feature/sv 50 clase player (#3)
    
    * Creada clase Player
    
    * SV-50 #time 2m #comment added database mapping and filename changed
    
    * SV-50 #time 1m #comment eliminated main.py for merge issues
    
    * SV-51 #time 10m #comment setting database, nothing to test. #done
    
    * SV-20 #time 1h #comment Creando Clase User #done
    
    SV-20 #time 2m #comment added Matches attribute for User class
    
    * SV-22 #time 30m #comment Register endpoint tested and reviewed. #done
    
    SV-22 #time 3h #comment register endpoint completed, not tested yet
    
    SV-22 #time 1m #comment Format changes
    
    * SV-23 #time 2h #comment Login endpoint tested and reviewed. #done" (#2)
    
    * SV-50 #time 10m #comment Creada clase Player
    
    Co-authored-by: Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
    Co-authored-by: csralvall <46329881+csralvall@users.noreply.github.com>
    
    Co-authored-by: Matidp <matiasdepascuale@gmail.com>
    Co-authored-by: Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
    Co-authored-by: csralvall <46329881+csralvall@users.noreply.github.com>
    Co-authored-by: Matidp <61301073+Matidp@users.noreply.github.com>

--- backend/crud.py
+++ backend/crud.py
@@ -1,1 +1,16 @@
+from pony.orm import *
 from .database import *
+
+@db_session
+def create_user(email: str, username: str, password: str):
+    try:
+        User(Email=email, Username=username, Password=password)
+        return True
+    except Exception:
+        return False
+
+
+#needed for testing
+@db_session
+def delete_data(table):
+    delete(p for p in table)
