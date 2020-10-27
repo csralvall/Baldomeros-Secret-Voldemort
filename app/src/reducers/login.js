b750b5f2920037b997502597a12d2b751f5741f7
Manuel Sosa <54127298+ManuelSosaMartinez@users.noreply.github.com>
2020-10-27 20:17:40 -0300

    Feature/sv 14 login interface (#10)
    SV-14 #comment Login Interface finished #done

--- app/src/reducers/login.js
+++ app/src/reducers/login.js
@@ -1,7 +1,16 @@
-const loginReducer = (state = false, action) => {
+import { act } from "react-dom/test-utils";
+import React from "react";
+
+const loginReducer = (state = {token: 0, logged_in: false, username :"", id:0}, action) => {
   switch (action.type) {
     case "LOGIN":
-      return !state;
+      if (action.payload.token == 145)
+        return {token : action.payload.token,
+                logged_in: true,
+                username: action.payload.username,
+                id: action.payload.id};
+      else
+        return state;    
     default:
       return state;
   }
