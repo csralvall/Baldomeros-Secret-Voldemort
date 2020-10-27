57eebc7a54d15f845165827493f5800f5f3bc85f
csralvall <46329881+csralvall@users.noreply.github.com>
2020-10-27 12:53:53 -0300

    Hotfix/file organization (#9)
    * users endpoints moved to properly folder
    
    * crud and database moved to db
    
    * corrected typos and reordered tests
    
    * created routers to keep api clear
    
    * introduced modifications to work with api/routers
    
    * moved files to proper directories
    
    * updated import to new directory structure
    
    * new names given to test classes
    
    * Corrected return

--- backend/tests/test_SV-34.py
+++ backend/tests/test_SV-34.py
@@ -1,11 +1,28 @@
 import unittest
-from crud import user_is_registred, check_email, check_username
+from backend.db.crud import *
 
+class TestCrudAllowedCases(unittest.TestCase):
 
-class TestCrud(unittest.TestCase):
+    def setUp(self):
+        create_user("mati@gmail.com", "mati", "123456")
+
+    def tearDown(self):
+        delete_user("mati@gmail.com", "mati", "123456")
 
     def test_user_is_registred_1(self):
         self.assertTrue(user_is_registred("mati", "123456"))
+
+    def test_check_username_1(self):
+        self.assertTrue(check_username("mati"))
+
+    def test_check_mail_1(self):
+        self.assertTrue(check_email("mati@gmail.com"))
+
+    def test_get_user(self):
+        self.assertEqual(get_user("mati", "123456")['Username'],  "mati")
+
+
+class TestCrudBadCases(unittest.TestCase):
 
     def test_user_is_registred_2(self):
         self.assertFalse(user_is_registred("mati", "12345"))
@@ -19,17 +36,11 @@ class TestCrud(unittest.TestCase):
     def test_user_is_registred_5(self): 
         self.assertFalse((user_is_registred("", "")))    
     
-    def test_check_username_1(self):
-        self.assertTrue(check_username("mati"))
-
     def test_check_username_2(self):
         self.assertFalse(check_username("mat"))
 
     def test_check_username_3(self):
         self.assertFalse(check_username(""))
-
-    def test_check_mail_1(self):
-        self.assertTrue(check_email("mati@gmail.com"))
 
     def test_check_mail_2(self):
         self.assertFalse(check_email("mati@gmail"))
@@ -37,11 +48,9 @@ class TestCrud(unittest.TestCase):
     def test_check_mail_3(self):
         self.assertFalse(check_email(""))
 
-    def test_get_user(self):
-        self.assertEqual(get_user("mati", "123456"), {"Id": 12, "Username": "mati"})
-
     def test_get_user_2(self):
         self.assertEqual(get_user("matii", "asfa"), None)
+
 
 if __name__ == '__main__':
     unittest.main()
