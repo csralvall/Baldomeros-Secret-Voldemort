f7b69327afe0e68c4732acaf7faec5b8526d57f8
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2020-10-26 22:38:44 -0300

    crud and database moved to db

---
+++ backend/db/crud.py
@@ -0,0 +1,60 @@
+from pony.orm import db_session, select, count
+from backend.db.database import *
+
+@db_session #Bool
+def user_is_registred(name, upassword):
+    try:
+        u = User.get(Username = name, Password = upassword)
+        if u:
+            return True
+        else: 
+            return False
+    except Exception:
+        return False
+
+@db_session
+def check_username(username):
+    try: 
+        u = User.exists(Username=username)
+        return u 
+    except Exception:
+        return False
+
+@db_session
+def check_email(email):
+    try: 
+        u = User.exists(Email=email)
+        return u 
+    except Exception:
+        pass
+        #return False
+
+@db_session #get the User object.
+def get_user(username, password):
+    user = User.get(Username=username, Password=password)
+    if user is not None:
+        user = user.to_dict("Id Username")
+    return user
+
+
+@db_session
+def create_user(email: str, username: str, password: str):
+    try:
+        User(Email=email, Username=username, Password=password)
+        return True
+    except Exception:
+        return False
+
+
+#needed for testing
+@db_session
+def delete_data(table):
+    delete(p for p in table)
+
+@db_session
+def delete_user(email, username, password):
+    user = User.get(Email=email, Username=username, Password=password)
+    if user is not None:
+        user.delete()
+
+    return user
---
+++ backend/db/database.py
@@ -0,0 +1,62 @@
+from pony.orm import *
+
+db = Database()
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)  
+
+
+Status = {0: "Joinable",
+          1: "In Game",
+          2: "Finished"}
+
+BoardType = {0: "5-6",
+             1: "7-8",
+             2: "9-10"}
+
+
+SecretRolDiccionary = {0:"Voldemort",
+                       1: "Death Eater",
+                       2: "Order of The Phoenix"}
+
+GovRolDiccionary = {0: "HeadMaster",
+                    1: "Magic Minister",
+                    2: "Citizen"}
+
+class Match(db.Entity):
+    Id = PrimaryKey(int, auto=True)
+    MaxPlayers = Required(int, min=5, max=10)
+    MinPlayers = Required(int, min=5, max=10)
+    Status = Required(int, min=0, max=2)
+    BoardType = Required(int, min=0, max=2)
+    LastMinister = Optional(int)
+    Players = Set('Player')
+    Board = Optional('Board')
+    Creator = Required('User')
+    
+class Board(db.Entity):
+    Id = PrimaryKey(int, auto=True)
+    BoardType = Required(int, min=0, max=2)
+    PhoenixProclamations = Optional(int, min=0, max=5)
+    DeathEaterProclamations = Optional(int, min=0, max=5)
+    FailedElectionsCount = Optional(int)
+    Match = Required(Match)
+
+class Player(db.Entity):
+    PlayerId = PrimaryKey(int, auto=True)
+    Position = Required(int, min=0, max=5)
+    SecretRol = Required(int, min=0, max=2)
+    GovRol = Required(int, min=0, max=2)
+    IsDead = Required(bool)
+    UserId = Optional('User')
+    MatchId = Optional('Match')
+
+class User(db.Entity):
+    Id = PrimaryKey(int, auto=True)
+    Email = Required(str, unique=True)
+    Username = Required(str, max_len=30, unique=True)
+    Password = Required(str, hidden=True, max_len=30)
+    Players = Set('Player')
+    Matches = Set('Match')
+
+
+db.generate_mapping(create_tables=True)  
+
