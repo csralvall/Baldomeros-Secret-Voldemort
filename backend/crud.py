e6192a74017a5d7db9b90dcf9454dff7dc334144
Feltes <joacofeltes@gmail.com>
2020-10-27 22:14:06 -0300

    SV-25 #time 50m #comment now add_match_db calls to 3 functions

--- backend/crud.py
+++ backend/crud.py
@@ -46,35 +46,59 @@ def create_user(email: str, username: str, password: str):
         return False
 
 @db_session
-def add_match_db(minp,maxp,uhid):
+def add_match(minp,maxp,creator):
     try:
-        creator= User[uhid]
         newmatch = Match(MaxPlayers=maxp,
             MinPlayers=minp,
             Status=0,
-            BoardType=0, #Por ahora hardcodeado
-            LastMinister = 0, #Cambia cuando empieza la partida
+            BoardType=0, #hardcoded
+            LastMinister = 0, #Changes when the match starts
             Creator = creator)
+        return newmatch
+    except :
+        return None
 
-        #inicializamos el tablero
-        Board(BoardType = newmatch.BoardType,
-            PhoenixProclamations = 0,
-            DeathEaterProclamations = 0,
-            FailedElectionsCount = 0,
-            Match = newmatch
-        )
-        #agregamos al creador a la tabla player
-        Player(Position = 0,
-            SecretRol = 0, #definir en el inicio de partida
-            GovRol = 0, #definir en el inicio de partida
-            IsDead = False,
-            UserId = creator,
-            MatchId = newmatch
-        )
-        return True
-    except Exception:
-        return False
+@db_session
+def add_board(newmatch):
+    newboard = Board(BoardType = newmatch.BoardType,
+        PhoenixProclamations = 0,
+        DeathEaterProclamations = 0,
+        FailedElectionsCount = 0,
+        Match = newmatch)
+    return newboard
 
+@db_session
+def add_user_in_match(user, matchid, position):
+    mymatch = Match[matchid]
+    newplayer = Player(Position = position,
+        SecretRol = 0, #Changes when the match starts
+        GovRol = 0, #Changes when the match starts
+        IsDead = False,
+        UserId = user,
+        MatchId = mymatch)
+    return newplayer
+
+@db_session
+def add_match_db(minp,maxp,uhid):
+    if(minp>maxp):
+        return None
+    try:
+        creator= User[uhid]
+    except :
+        return None
+    
+    match = add_match(minp,maxp,creator)
+    if match is not None:
+        matchId= match.to_dict("Id")["Id"]
+        add_board(match)
+        player = add_user_in_match(creator, matchId, 0)# add the creeator to player table 
+        match_and_player = {
+            "Match_id": matchId,
+            "Player_id": player.to_dict("PlayerId")["PlayerId"]
+        }
+        return match_and_player
+    else:
+        return None
 
 #needed for testing
 @db_session
