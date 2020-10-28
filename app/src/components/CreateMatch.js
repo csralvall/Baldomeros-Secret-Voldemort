f835372f7fc49141ebb1b430766b4cc7e5fbc7c3
Guido Ivetta <guidoivetta@gmail.com>
2020-10-28 17:31:53 -0300

    Feature/sv 15 create match (#16)

---
+++ app/src/components/CreateMatch.js
@@ -0,0 +1,162 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useSelector } from "react-redux";
+import { Slider, Rail, Handles, Tracks, Ticks } from "react-compound-slider";
+import "./CreateMatch.css";
+import {
+  sliderStyle,
+  railStyle,
+  Handle,
+  Track,
+  Tick,
+} from "./CreateMatchSliderProps";
+
+function CreateMatch() {
+  const isLogged = useSelector((state) => state.user.logged_in);
+  const userID = useSelector((state) => state.user.id);
+  const [isCreateMatchSuccess, setIsCreateMatchSuccess] = useState(false);
+  const [minMaxPlayers, setMinMaxPlayers] = useState({
+    minPlayers: 5,
+    maxPlayers: 10,
+  });
+  const [matchProps, setMatchProps] = useState({
+    userID: userID,
+    minPlayers: minMaxPlayers.minPlayers,
+    maxPlayers: minMaxPlayers.maxPlayers,
+  });
+
+  const updateMinMaxPlayers = (values) => {
+    setMinMaxPlayers({ minPlayers: values[0], maxPlayers: values[1] });
+  };
+
+  const loaded = useRef(false);
+  useEffect(() => {
+    if (loaded.current) {
+      createMatch();
+    } else {
+      loaded.current = true;
+    }
+  }, [matchProps]);
+
+  const createMatch = async () => {
+    const url = "http://127.0.0.1:8000";
+
+    const formData = new FormData();
+    formData.append("userID", userID);
+    formData.append("minPlayers", minMaxPlayers.minPlayers);
+    formData.append("maxPlayers", minMaxPlayers.maxPlayers);
+
+    await fetch(url + "/game/new", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          alert("Could not Create Match. Unknown Error.");
+        } else {
+          setIsCreateMatchSuccess(true);
+          console.log(matchProps);
+        }
+      })
+      .catch(() => {
+        alert("Network Error");
+      });
+  };
+
+  const getMatchProps = (e) => {
+    e.preventDefault();
+    if (minMaxPlayers.minPlayers > minMaxPlayers.maxPlayers) {
+      alert("Min players can't be larger than max players!");
+    } else {
+      setMatchProps({
+        userID: userID,
+        minPlayers: minMaxPlayers.minPlayers,
+        maxPlayers: minMaxPlayers.maxPlayers,
+      });
+    }
+  };
+
+  const createMatchFormJSX = (
+    <div>
+      {isLogged && !isCreateMatchSuccess ? (
+        <div>
+          <h1 className="title">Create Match</h1>
+          <form onSubmit={getMatchProps} className="create-game-form">
+            <label>
+              Min Players
+              <h1>{minMaxPlayers.minPlayers}</h1>
+            </label>
+            <label>
+              Max Players
+              <h1>{minMaxPlayers.maxPlayers}</h1>
+            </label>
+            <button className="create-button" type="submit">
+              Create Match
+            </button>
+          </form>
+          <Slider
+            rootStyle={sliderStyle}
+            domain={[5, 10]}
+            step={1}
+            mode={1}
+            values={[5, 10]}
+            onUpdate={updateMinMaxPlayers}
+          >
+            <Rail>
+              {({ getRailProps }) => (
+                <div style={railStyle} {...getRailProps()} />
+              )}
+            </Rail>
+            <Handles>
+              {({ handles, getHandleProps }) => (
+                <div className="slider-handles">
+                  {handles.map((handle) => (
+                    <Handle
+                      key={handle.id}
+                      handle={handle}
+                      getHandleProps={getHandleProps}
+                    />
+                  ))}
+                </div>
+              )}
+            </Handles>
+            <Tracks left={false} right={false}>
+              {({ tracks, getTrackProps }) => (
+                <div className="slider-tracks">
+                  {tracks.map(({ id, source, target }) => (
+                    <Track
+                      key={id}
+                      source={source}
+                      target={target}
+                      getTrackProps={getTrackProps}
+                    />
+                  ))}
+                </div>
+              )}
+            </Tracks>
+            <Ticks count={5}>
+              {({ ticks }) => (
+                <div className="slider-ticks">
+                  {ticks.map((tick) => (
+                    <Tick key={tick.id} tick={tick} count={ticks.length} />
+                  ))}
+                </div>
+              )}
+            </Ticks>
+          </Slider>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+
+  return (
+    <div>
+      {createMatchFormJSX}
+      {!isLogged && <h1>Please Log In to create match</h1>}
+      {isCreateMatchSuccess && <h1>Success!</h1>}
+    </div>
+  );
+}
+
+export default CreateMatch;
---
+++ app/src/components/CreateMatchSliderProps.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+export const sliderStyle = {
+  position: "relative",
+  width: "50%",
+  left: "25%",
+  height: 80,
+};
+
+export const railStyle = {
+  position: "absolute",
+  width: "100%",
+  height: 10,
+  marginTop: 35,
+  borderRadius: 5,
+  backgroundColor: "#8B9CB6",
+};
+
+export function Handle({ handle: { id, value, percent }, getHandleProps }) {
+  return (
+    <div
+      style={{
+        left: `${percent}%`,
+        position: "absolute",
+        marginLeft: -15,
+        marginTop: 25,
+        zIndex: 2,
+        width: 30,
+        height: 30,
+        border: 0,
+        textAlign: "center",
+        cursor: "pointer",
+        borderRadius: "50%",
+        backgroundColor: "#2C4870",
+        color: "#333",
+      }}
+      {...getHandleProps(id)}
+    ></div>
+  );
+}
+
+export function Track({ source, target, getTrackProps }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        height: 10,
+        zIndex: 1,
+        marginTop: 35,
+        backgroundColor: "#546C91",
+        borderRadius: 5,
+        cursor: "pointer",
+        left: `${source.percent}%`,
+        width: `${target.percent - source.percent}%`,
+      }}
+      {
+        ...getTrackProps() /* this will set up events if you want it to be clickeable (optional) */
+      }
+    />
+  );
+}
+
+export function Tick({ tick, count }) {
+  return (
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          marginTop: 52,
+          marginLeft: -0.5,
+          width: 1,
+          height: 8,
+          backgroundColor: "silver",
+          left: `${tick.percent}%`,
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          marginTop: 60,
+          fontSize: 10,
+          textAlign: "center",
+          marginLeft: `${-(100 / count) / 2}%`,
+          width: `${100 / count}%`,
+          left: `${tick.percent}%`,
+        }}
+      >
+        {tick.value}
+      </div>
+    </div>
+  );
+}
