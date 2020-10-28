9b689b6f42996e1af5f23bb34801a09b4bd16467
Feltes <joacofeltes@gmail.com>
2020-10-27 22:14:45 -0300

    SV-25 #time 30m #comment more tests

--- backend/tests/test_SV-25.py
+++ backend/tests/test_SV-25.py
@@ -5,8 +5,9 @@ from ..database import *
 class TestAddMatch(unittest.TestCase):
 
     def setUp(self):
-        create_user("joaco@gmail.com","joaco","kajdsflaskfdjsla")
-    
+        delete_data(User)
+        create_user("exapmle@gmail.com","exapmle","password")
+        
     def tearDown(self):
         delete_data(Board)
         delete_data(Player)
@@ -14,27 +15,43 @@ class TestAddMatch(unittest.TestCase):
         delete_data(User)
 
     def test_ok1(self):
-        user = get_user("joaco","kajdsflaskfdjsla")
+        user = get_user("exapmle","password")
         userId = user['Id']
         self.assertTrue(add_match_db(5,5,userId))
 
     def test_ok2(self):
-        user = get_user("joaco","kajdsflaskfdjsla")
+        user = get_user("exapmle","password")
         userId = user['Id']
         self.assertTrue(add_match_db(6,7,userId))
 
     def test_fail_maxp(self):
-        user = get_user("joaco","kajdsflaskfdjsla")
+        user = get_user("exapmle","password")
         userId = user['Id']
         self.assertFalse(add_match_db(5,1,userId))
 
+    def test_fail_maxp2(self):
+        user = get_user("exapmle","password")
+        userId = user['Id']
+        self.assertFalse(add_match_db(5,11,userId))
+
     def test_fail_minp(self):
-        user = get_user("joaco","kajdsflaskfdjsla")
+        user = get_user("exapmle","password")
         userId = user['Id']
         self.assertFalse(add_match_db(2,6,userId))
 
+    def test_fail_minp2(self):
+        user = get_user("exapmle","password")
+        userId = user['Id']
+        self.assertFalse(add_match_db(11,12,userId))
+    
+    def test_fail_minp_and_maxp(self):
+        user = get_user("exapmle","password")
+        userId = user['Id']
+        self.assertFalse(add_match_db(7,6,userId))
+
+    #there is a case that this test fails if we reach that id
     def test_fail_id(self):
-        self.assertFalse(add_match_db(2,6,'3252135'))
+        self.assertFalse(add_match_db(2,6,99999999999))
 
 
 if __name__ == '__main__':
