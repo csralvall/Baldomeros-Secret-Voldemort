b2b8e77aa29caad0c9467e99310c97aa31e47e74
rortiz <rodrig00rtiz0422@gmail.com>
2020-10-27 22:55:20 -0300

    SV-35 #comment working on joinmatch

--- backend/api/routers/joinmatch.py
+++ backend/api/routers/joinmatch.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from backend.db.crud import *
 
+app = FastAPI()
+
+def there_is_space(mid): return True
 
 @app.post("/game/{gid}")
 async def join_game(mid: int, user: int): 
 
-    if (there_is_space(mid))
-        
+    if there_is_space(mid):
+
         playerobj = add_user_in_match(user, mid, 5) #hardcodeado position should be there
 
-        if (playerobj is not None)
+        if (playerobj is not None):
 
             playerdic = {
                 "Match_id": mid['Id'],
@@ -17,8 +20,12 @@ async def join_game(mid: int, user: int):
             }
 
             return playerdic
-        else 
+
+        else: 
+
             raise HTTPException(status_code=404, detail="couldnt add the user")
-    else 
+
+    else: 
+
         raise HTTPException(status_code=404, detail="there is no space")
 
