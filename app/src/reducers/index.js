cbb3168b3fcd48f3f7a5be4070d98030701eeebd
Guido Ivetta <guidoivetta@gmail.com>
2020-10-29 21:53:35 -0300

    Hotfix/match redux (#18)

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,8 +1,10 @@
 import loginReducer from "./login";
+import StoreMatchReducer from "./storeMatch";
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
   user: loginReducer,
+  match: StoreMatchReducer,
 });
 
 export default reducers;
