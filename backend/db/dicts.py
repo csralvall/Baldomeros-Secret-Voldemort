532ed5d63bc63bf5761a6dba90bbb6e8466914cc
csralvall <46329881+csralvall@users.noreply.github.com>
2020-10-31 15:10:04 -0300

    Feature/sv 42 voting endpoint (#22)

---
+++ backend/db/dicts.py
@@ -0,0 +1,21 @@
+
+Status = {0: "Joinable",
+          1: "In Game",
+          2: "Finished"}
+
+BoardType = {0: "5-6",
+             1: "7-8",
+             2: "9-10"}
+
+
+SecretRolDiccionary = {0:"Voldemort",
+                       1: "Death Eater",
+                       2: "Order of The Phoenix"}
+
+GovRolDiccionary = {0: "Head Master",
+                    1: "Magic Minister",
+                    2: "Citizen"}
+
+VoteType = {0: 'nox',
+            1: 'lumos',
+            2: 'missing vote'}
