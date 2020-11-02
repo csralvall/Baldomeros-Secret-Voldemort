f416c02b057fe4148af6e33ea9db10739ef7a014
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2020-11-02 14:32:43 -0300

    match identifier unified for all routes

--- backend/api/routers/game.py
+++ backend/api/routers/game.py
@@ -6,29 +6,29 @@ from typing import Optional
 
 router = APIRouter()
 
-@router.put("/game/{gid}/player/{pid}", tags=["Game"], status_code=200)
+@router.put("/game/{mid}/player/{pid}", tags=["Game"], status_code=200)
 async def vote_candidate(
-        gid: int = Path(..., title="The ID of the current game"),
+        mid: int = Path(..., title="The ID of the current game"),
         pid: int = Path(..., title="The ID of the player who votes"),
         vote: str = Query(..., regex="^(nox|lumos)$")):
 
-        if not check_match(gid):
+        if not check_match(mid):
             raise HTTPException(status_code=404, detail="Match not found")
 
-        if not check_player_in_match(gid,pid):
+        if not check_player_in_match(mid,pid):
             raise HTTPException(status_code=404, detail="Player not found")
 
         vote_director(pid, vote)
 
-        player_votes = get_player_votes(gid)
+        player_votes = get_player_votes(mid)
 
         if 'missing vote' not in player_votes.values():
-            set_next_minister(gid)
-            if compute_election_result(gid) == 'lumos':
-                enact_proclamation(gid,'death eater')
-            restore_election(gid)
+            set_next_minister(mid)
+            if compute_election_result(mid) == 'lumos':
+                enact_proclamation(mid,'death eater')
+            restore_election(mid)
 
-        winner = is_victory_from(gid)
+        winner = is_victory_from(mid)
 
         return winner
 
