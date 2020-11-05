efaf1f65f27946736d6f15162613434dc1550df0
Guido Ivetta <guidoivetta@gmail.com>
2020-11-05 19:57:29 -0300

    Feature/sv 81 lobby redesign (#34)
    * SV-81 #comment Redesigned Lobby and changed React directory structure
    
    * SV-81 #message Added Lobby tests

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Nav from "./Nav";
+import Nav from "./components/Nav";
 import Lobby from "./components/Lobby";
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
--- app/src/components/Lobby.js
+++ app/src/components/Lobby.js
@@ -1,29 +1,120 @@
 import React from "react";
-import JoinMatch from "./JoinMatch";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import harry2 from "../media/roles/harry2.png";
+import ron from "../media/roles/ron.png";
+import hermione from "../media/roles/hermione.png";
+import sirius from "../media/roles/sirius.png";
+import dumbledore from "../media/roles/dumbledore.png";
+import weasley1 from "../media/roles/weasley1.png";
+import weasley2 from "../media/roles/weasley2.png";
+import snapePhoenix from "../media/roles/snapePhoenix.png";
+import bellatrix from "../media/roles/bellatrix.png";
+import umbridge from "../media/roles/umbridge.png";
+import malfoyPadre from "../media/roles/malfoyPadre.png";
+import draco from "../media/roles/draco.png";
+import voldermort from "../media/roles/voldemort.png";
+import logo from "../media/logos/logo.png";
+import "./css/Lobby.css";
 
 function Lobby() {
   const user = useSelector((state) => state.user);
+  const history = useHistory();
 
   return (
     <div>
-      {user.logged_in ? (
-        <div>
-          <h1> Welcome, {user.username} </h1>
-          <Link to="/match/create">
-            <button>Create Game</button>
-          </Link>
-          <JoinMatch />
+      <img src={logo} className="logo" />
+      <div className="btns">
+        {user.logged_in ? (
+          <div>
+            <button
+              className="create-match-btn"
+              onClick={() => {
+                history.push("/match/create");
+              }}
+            >
+              Create Match
+            </button>
+            <button
+              className="join-match-btn"
+              onClick={() => {
+                history.push("/match/join");
+              }}
+            >
+              Join Match
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button
+              className="login-btn"
+              onClick={() => {
+                history.push("/login");
+              }}
+            >
+              Login
+            </button>
+            <button
+              className="sign-up-btn"
+              onClick={() => {
+                history.push("/signup");
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="img-tiles">
+        <div className="image-container">
+          <img src={harry2} />
+          <img className="transition-image" src={voldermort} />
         </div>
-      ) : (
-        <div>
-          <h1> You are not Logged In</h1>
-          <Link to="/login">
-            <h1> Login</h1>
-          </Link>
+        <div className="image-container">
+          <img src={ron} />
+          <img className="transition-image" src={voldermort} />
         </div>
-      )}
+        <div className="image-container">
+          <img src={hermione} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={malfoyPadre} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={dumbledore} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={weasley1} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={umbridge} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={sirius} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={weasley2} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={draco} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={snapePhoenix} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+        <div className="image-container">
+          <img src={bellatrix} />
+          <img className="transition-image" src={voldermort} />
+        </div>
+      </div>
     </div>
   );
 }
