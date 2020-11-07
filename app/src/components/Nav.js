b298ad0f6c86929719c63907d1841786fdfbf254
Manuel Sosa <54127298+ManuelSosaMartinez@users.noreply.github.com>
2020-11-07 14:06:53 -0300

    Feature/sv 86 logout react (#38)

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./css/Nav.css";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import logo from "../media/logos/logo.png";
+import { logout } from "../actions/login";
 
 function Nav() {
   const logged_in = useSelector((state) => state.user.logged_in);
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    if (logged_in) {
+      dispatch(logout());
+    }
+  };
   return (
     <div>
       <nav>
@@ -17,8 +24,8 @@ function Nav() {
           <Link to={!logged_in ? "/login" : "/Profile"} className="nav-link">
             <li>{!logged_in ? "Login" : "Profile"}</li>
           </Link>
-          <Link to={!logged_in ? "/signup" : ""} className="nav-link">
-            <li>{!logged_in ? "Sign Up" : ""}</li>
+          <Link to={!logged_in ? "/signup" : "/"} className="nav-link">
+            <li onClick={handleLogout}>{!logged_in ? "Sign Up" : "Log Out"}</li>
           </Link>
         </ul>
       </nav>
