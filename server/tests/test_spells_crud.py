333e3a1c53c1803200163fd4156bfb9fa7698dbb
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2020-11-14 13:09:13 -0300

    added new tests to avada-kedavra endpoint and crud functions

--- server/tests/test_spells_crud.py
+++ server/tests/test_spells_crud.py
@@ -40,7 +40,7 @@ class TestDeck(unittest.TestCase):
         self.assertEqual(get_player_id_from_username(self.match, "dfa"), None)
 
     def test_avada_kedavra(self):
-        create_user("bar@bar.com","bar","bar")
+        self.assertTrue(create_user("bar@bar.com","bar","bar"))
         new_userid = get_user("bar","bar")["Id"]
         add_user_in_match(new_userid, self.match, 2)
         player_id = get_player_id(self.match, new_userid)
@@ -52,6 +52,38 @@ class TestDeck(unittest.TestCase):
 
     def test_avada_kedavra_board_not_found(self):
         self.assertRaises(PlayerNotFound, avada_kedavra, self.board, -1)
+
+    def test_is_voldemort_dead(self):
+        self.assertTrue(create_user("bar@bar.com","bar","bar"))
+        new_userid = get_user("bar","bar")["Id"]
+        add_user_in_match(new_userid, self.match, 2)
+        player_id = get_player_id(self.match, new_userid)
+        change_player_rol(player_id, VOLDEMORT)
+        change_player_rol(self.playerid, PHOENIX)
+        self.assertEqual(get_player_rol(player_id),
+                        SecretRolDiccionary[VOLDEMORT])
+        self.assertEqual(get_player_rol(self.playerid),
+                        SecretRolDiccionary[PHOENIX])
+        avada_kedavra(self.board, player_id)
+        self.assertTrue(is_voldemort_dead(self.match))
+
+    def test_is_voldemort_dead_not_setted_voldemort(self):
+        change_player_rol(self.playerid, PHOENIX)
+        self.assertEqual(get_player_rol(self.playerid),
+                        SecretRolDiccionary[PHOENIX])
+        self.assertRaises(VoldemortNotFound, is_voldemort_dead, self.match)
+
+    def test_is_voldemort_dead_match_not_found(self):
+        self.assertRaises(MatchNotFound, is_voldemort_dead, self.match+1)
+
+    def test_set_death_eater_winner(self):
+        set_death_eater_winner(self.match)
+        self.assertEqual(get_match_status(self.match), Status[FINISHED])
+        self.assertEqual(check_winner(self.match), DEATH_EATER_WINNER)
+        
+
+    def test_set_death_eater_winner_match_not_found(self):
+        self.assertRaises(MatchNotFound, set_death_eater_winner, self.match+1)
 
 if __name__ == "__main__":
     unittest.main()
