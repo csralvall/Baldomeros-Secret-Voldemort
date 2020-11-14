a5c6a9f2e942c7d2a289aa36bcbdbc00eb9c9253
Matidp <61301073+Matidp@users.noreply.github.com>
2020-11-14 12:24:55 -0300

    hot fix tests endpoint

--- server/tests/test_set_candidate_director_endpoint.py
+++ server/tests/test_set_candidate_director_endpoint.py
@@ -1,4 +1,3 @@
-
 from fastapi.testclient import TestClient
 
 from server.db.crud import *
@@ -8,7 +7,7 @@ from server.main import app
 
 client = TestClient(app)
 
-def test_select_candidate_Player_not_exist():
+def test_vote_avada_kedavra():
     delete_data(Board)
     delete_data(Player)
     delete_data(Match)
@@ -20,18 +19,65 @@ def test_select_candidate_Player_not_exist():
     uid2 = get_user("bar", "bar")["Id"]
 
     match = add_match_db(5,7,uid1)
+    match_id = match['Match_id']
+    pid = match['Player_id']    
+    make_director(pid)
+    set_current_director(match_id,0)
 
-    mid = match['Match_id']
-    pid = 3
+    add_user_in_match(uid2, match_id, 2)
 
     response = client.patch(
-        f"/game/{mid}/director/{pid}"
+        f"/game/{match_id}/board/avada-kedavra?playername=bar"
+    )
+
+    assert response.status_code == 200
+    assert response.json() == "bar is dead"
+
+def test_vote_avada_kedavra_minister_autokill():
+    delete_data(Board)
+    delete_data(Player)
+    delete_data(Match)
+    delete_data(User)
+
+    create_user("foo@gmail.com", "foo", "foo")
+    create_user("bar@gmail.com", "bar", "bar")
+    uid1 = get_user("foo", "foo")["Id"]
+    uid2 = get_user("bar", "bar")["Id"]
+
+    match_id = add_match_db(5,7,uid1)['Match_id']
+
+    add_user_in_match(uid2, match_id, 2)
+
+    set_gob_roles(match_id)
+
+    minister = get_minister_username(match_id)
+
+    response = client.patch(
+        f"/game/{match_id}/board/avada-kedavra?playername={minister}"
+    )
+
+    assert response.status_code == 403
+    assert response.json()['detail'] == "You can't kill yourself"
+
+def test_vote_avada_kedavra_bad_playername():
+    delete_data(Board)
+    delete_data(Player)
+    delete_data(Match)
+    delete_data(User)
+
+    create_user("foo@gmail.com", "foo", "foo")
+    uid1 = get_user("foo", "foo")["Id"]
+
+    match_id = add_match_db(5,7,uid1)['Match_id']
+
+    response = client.patch(
+        f"/game/{match_id}/board/avada-kedavra?playername=asdad"
     )
 
     assert response.status_code == 404
-    assert response.json()['detail'] == "Player not found"
+    assert response.json()['detail'] == "Resource not found"
 
-def test_select_candidate_Match_not_exist():
+def test_vote_avada_kedavra_bad_match_id():
     delete_data(Board)
     delete_data(Player)
     delete_data(Match)
@@ -42,62 +88,89 @@ def test_select_candidate_Match_not_exist():
     uid1 = get_user("foo", "foo")["Id"]
     uid2 = get_user("bar", "bar")["Id"]
 
-    match = add_match_db(5,7,uid1)
-    pid = 1
-    mid = 99
+    match_id = add_match_db(5,7,uid1)['Match_id']
+
+    add_user_in_match(uid2, match_id, 2)
+
+    match_id += 1
 
     response = client.patch(
-        f"/game/{mid}/director/{pid}"
+        f"/game/{match_id}/board/avada-kedavra?playername=bar"
     )
 
     assert response.status_code == 404
     assert response.json()['detail'] == "Match not found"
 
-def test_select_candidate_resource_not_found():
+def test_vote_avada_kedavra_empty_match_id():
     delete_data(Board)
     delete_data(Player)
     delete_data(Match)
     delete_data(User)
 
-    create_user("foo@gmail.com", "foo", "foo")
-    create_user("bar@gmail.com", "bar", "bar")
-    create_user("m@gmail.com", "m", "m")
-    uid1 = get_user("foo", "foo")["Id"]
-    uid2 = get_user("bar", "bar")["Id"]
-    uid3 = get_user("m", "m")["Id"]
-
-    match = add_match_db(5,7,uid1)
-    mid = match['Match_id']
-    pid2 = add_user_in_match(uid2, mid, 2).PlayerId
-    match2 = add_match_db(5,7,uid3)
-    pid3 = 3 #playerid uid3 in match2
-
     response = client.patch(
-        f"/game/{mid}/director/{pid3}"
+        f"/game//board/avada-kedavra?playername=bar"
     )
 
     assert response.status_code == 404
-    assert response.json()['detail'] == "Player not found"
+    assert response.json()['detail'] == "Not Found"
 
-def test_select_candidate_t():
+def test_vote_avada_kedavra_char_match_id():
+    delete_data(Board)
+    delete_data(Player)
+    delete_data(Match)
+    delete_data(User)
+
+    response = client.patch(
+        f"/game/afda/board/avada-kedavra?playername=bar"
+    )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == "value is not a valid integer"
+
+def test_vote_avada_kedavra_without_player_name():
+    delete_data(Board)
+    delete_data(Player)
+    delete_data(Match)
+    delete_data(User)
+
+    response = client.patch(
+        f"/game/123/board/avada-kedavra"
+    )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == "field required"
+
+
+def test_vote_avada_kedavra_bad_player_name_field():
+    delete_data(Board)
+    delete_data(Player)
+    delete_data(Match)
+    delete_data(User)
+
+    response = client.patch(
+        f"/game/123/board/avada-kedavra?player=bar"
+    )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == "field required"
+
+
+def test_vote_avada_kedavra_empty_player_name_field():
     delete_data(Board)
     delete_data(Player)
     delete_data(Match)
     delete_data(User)
 
     create_user("foo@gmail.com", "foo", "foo")
-    create_user("bar@gmail.com", "bar", "bar")
-    create_user("m@gmail.com", "m", "m")
     uid1 = get_user("foo", "foo")["Id"]
-    uid2 = get_user("bar", "bar")["Id"]
 
-    match = add_match_db(5,7,uid1)
-    mid = match['Match_id']
-    pid2 = add_user_in_match(uid2, mid, 2).PlayerId
+    match_id = add_match_db(5,7,uid1)['Match_id']
 
     response = client.patch(
-        f"/game/{mid}/director/{pid2}"
+        f"/game/{match_id}/board/avada-kedavra?playername="
     )
 
-    assert response.status_code == 200
-    assert response.json() == "bar is the candidate to director"
+    assert response.status_code == 404
+    assert response.json()['detail'] == "Resource not found"
+
+
