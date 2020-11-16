506b4a20664912af660b96f04ef8e52576c6e561
rortiz04 <50550642+rortiz04@users.noreply.github.com>
2020-11-16 16:51:26 -0300

    Feature/sv 76 adivinacion (#65)

---
+++ server/tests/test_adivination.py
@@ -0,0 +1,55 @@
+import unittest 
+from server.db.crud import *
+from server.db.database import *
+from server.tests.helpers import *
+
+class Testadivination(unittest.TestCase):
+
+    def setUp(self):
+
+        delete_data(Board)
+        delete_data(Player)
+        delete_data(Match)
+        delete_data(User)
+
+        create_user("example1@gmail.com","example1", "password")
+
+        self.user1id = get_user("example1","password")['Id']
+    
+        self.game1 = add_match_db(5, 5, self.user1id) 
+
+        self.matchid1 = self.game1["Match_id"]
+
+        self.board_id = get_match_board_id(self.matchid1)
+
+    def tearDown(self):
+
+        delete_data(Board)
+        delete_data(Player)
+        delete_data(Match)
+        delete_data(User)
+
+#--------------------------- Test get_min_players ----------------------------
+
+    def test_adivnination(self):
+        enact_proclamation(self.matchid1, "death eater")
+        enact_proclamation(self.matchid1, "death eater")
+        enact_proclamation(self.matchid1, "death eater")
+
+        self.assertEqual(unlock_spell(self.matchid1), ADIVINATION) 
+
+        adivination(self.board_id)
+
+        self.assertEqual(get_board_status(self.board_id)['spell'], None)
+
+    def test_adivnination2(self):
+        self.assertRaises(BoardNotFound, adivination, self.board_id + 1)
+
+    def test_adivnination3(self):
+        enact_proclamation(self.matchid1, "death eater")
+        enact_proclamation(self.matchid1, "death eater")
+        enact_proclamation(self.matchid1, "death eater")
+        enact_proclamation(self.matchid1, "death eater")
+
+        self.assertNotEqual(unlock_spell(self.matchid1), ADIVINATION) 
+
