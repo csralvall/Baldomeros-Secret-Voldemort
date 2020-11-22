a5b1956b7c5cc8c6cf98dee249cc0dc64a7692d3
Feltes <joacofeltes@gmail.com>
2020-11-22 16:31:01 -0300

    get_posible_director function untiested, with multiple number of players alive

--- server/tests/test_SV-87.py
+++ server/tests/test_SV-87.py
@@ -16,6 +16,7 @@ class TestSV87(unittest.TestCase):
         create_user("example4@gmail.com","example4","password")
         create_user("example5@gmail.com","example5","password")
         create_user("example6@gmail.com","example6","password")
+        self.userid = get_user("example","password")['Id']
         self.user1id = get_user("example2","password")['Id']
         self.user2id = get_user("example3","password")['Id']
         self.user3id = get_user("example4","password")['Id']
@@ -25,12 +26,13 @@ class TestSV87(unittest.TestCase):
         m_p2 = add_match_db(5,5,self.user2id)
         self.matchid = m_p["Match_id"]
         self.matchid2 = m_p2["Match_id"]
-        player1 = add_user_in_match(self.user1id,self.matchid,1)
         player2 = add_user_in_match(self.user2id,self.matchid,2)
         player3 = add_user_in_match(self.user3id,self.matchid,3)
         player4 = add_user_in_match(self.user4id,self.matchid,4)
         player5 = add_user_in_match(self.user5id,self.matchid,5)
-        self.player1id = player1.to_dict("PlayerId")["PlayerId"]
+        player = add_user_in_match(self.userid,self.matchid,6)
+        self.player1id= m_p["Player_id"]
+        self.playerid = player.to_dict("PlayerId")["PlayerId"]
         self.player2id = player2.to_dict("PlayerId")["PlayerId"]
         self.player3id = player3.to_dict("PlayerId")["PlayerId"]
         self.player4id = player4.to_dict("PlayerId")["PlayerId"]
@@ -87,6 +89,23 @@ class TestSV87(unittest.TestCase):
         change_player_rol(self.player4id,1) 
         self.assertEqual(get_death_eater_players_in_match(self.matchid),{'Voldemort':'example2', 'Death Eater': ['example3','example4','example5']})
         
+    #--------------------------- Test get_posible_directors ----------------------------
+
+    def test_get_posible_directors(self):
+        make_magician(self.player1id)
+        make_magician(self.playerid)
+        make_magician(self.player2id)
+        make_magician(self.player3id)
+        make_magician(self.player4id)
+        make_magician(self.player5id)
+        make_minister(self.player1id)#example2 es el ministro
+        self.assertEqual(get_posible_directors(self.matchid), {"posible directors": ['example3','example4','example5','example6','example']})
+        make_ex_minister(self.player2id)#example3 es exministro,no tienen que mostrarlo porque son 6.
+        self.assertEqual(get_num_players(self.matchid),6)
+        self.assertEqual(get_posible_directors(self.matchid), {"posible directors": ['example4','example5','example6','example']})
+        kill_player(self.playerid)#ahora se tiene que mostar el 3
+        self.assertEqual(get_posible_directors(self.matchid), {"posible directors": ['example3','example4','example5','example6']})
+
 
 if __name__ == '__main__':
     unittest.main()
