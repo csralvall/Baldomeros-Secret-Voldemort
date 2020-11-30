a001776501407ab454a40f83ff43de31b649385e
guidoivetta <guidoivetta@gmail.com>
2020-11-30 15:59:20 -0300

    SV-102 #message FInished Component

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,11 +1,16 @@
 import React from "react";
+import ProfileInfo from "./ProfileInfo";
 import ChangePassword from "./ChangePassword";
 import ChangeEmail from "./ChangeEmail";
 
 function Profile() {
   return (
-    <div className="sign-up-module">
-      <h1 className="sign-up-title">My Profile</h1>
+    <div className="profile-module">
+      <h1 className="profile-title">My Profile</h1>
+      <hr className="line" />
+      <ProfileInfo />
+      <hr className="line" />
+      <h1 className="change-info-title">Change Info</h1>
       <ChangePassword />
       <ChangeEmail />
     </div>
